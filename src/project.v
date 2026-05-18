275b618ded4e612e2efc8f52da617119f26f8796
Kristaps Jurkans <kris.jurkans@gmail.com>
2026-05-18 18:37:32 +0100

    fix: update defaults

--- src/project.v
+++ src/project.v
@@ -9,7 +9,7 @@
 module artwork();
 endmodule
 
-module tt_um_example (
+module tt_um_krisjdev_artwork (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
